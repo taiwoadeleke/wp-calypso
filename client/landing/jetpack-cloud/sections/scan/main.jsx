129a33ed5b38f1ffe9f6714d1c9ad7dc03ba4b48
Echo G <ChaosExAnima@users.noreply.github.com>
2020-03-04 12:01:01 -0500

    Adds logic to switch scan states.

--- client/landing/jetpack-cloud/sections/scan/main.jsx
+++ client/landing/jetpack-cloud/sections/scan/main.jsx
@@ -10,15 +10,54 @@ import { connect } from 'react-redux';
 import { getSelectedSiteId } from 'state/ui/selectors';
 
 class ScanPage extends Component {
+	renderScanOkay() {
+		return <p>Everything is okay!</p>;
+	}
+
+	renderScanning() {
+		return <p>Scanning!</p>;
+	}
+
+	renderThreats() {
+		return <p>Threats found.</p>;
+	}
+
+	renderScanError() {
+		return <p>There is an error with the scan.</p>;
+	}
+
+	renderScanState() {
+		switch ( this.props.scanState ) {
+			case 'okay':
+				return this.renderScanOkay();
+			case 'scanning':
+				return this.renderScanning();
+			case 'threats':
+				return this.renderThreats();
+			case 'error':
+				return this.renderScanError();
+		}
+	}
+
 	render() {
-		return <div>Welcome to the scan page for site { this.props.siteId }</div>;
+		return (
+			<div>
+				Welcome to the scan page for site { this.props.siteId }
+				{ this.renderScanState() }
+			</div>
+		);
 	}
 }
 
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
 
+	// TODO: Get state from actual API.
+	const params = new URL( document.location ).searchParams;
+	const scanState = params.get( 'scan-state' ) || 'okay';
+
 	return {
 		siteId,
+		scanState,
 	};
 } )( ScanPage );
